80a3789813302383e54400f6a5568631f9d92fa7
Branco <branco_937@hotmail.com>
2020-05-16 13:49:09 -0500

    Initial commit

---
+++ Farmacia/Controllers/RProductoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Farmacia.Controllers
+{
+    public class RProductoController : Controller
+    {
+        // GET: RProducto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Farmacia/StoredProcedure/RProductoSP.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Farmacia.StoredProcedure
+{
+    public class RProductoSP
+    {
+    }
+}
